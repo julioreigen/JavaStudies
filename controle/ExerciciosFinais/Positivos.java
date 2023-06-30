cc13ff41f3f5f3d0e005eb5571443e6b6fd6bf95
Julio <juliobrito.tec@gmail.com>
2023-06-30 15:56:33 -0300

    Realizados os exercícios finais do módulo de controle

---
+++ controle/ExerciciosFinais/Positivos.java
@@ -0,0 +1,31 @@
+package controle.ExerciciosFinais;
+
+import java.util.Scanner;
+
+/*
+programa que enquanto estiver recebendo números positivos, imprime no console
+a soma dos números inseridos,
+caso receba um número negativo, encerre o programa.
+Tente utilizar a estrutura do while.
+ */
+public class Positivos {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double valor;
+        double soma = 0;
+
+        do {
+            System.out.print("Digite um número: ");
+            valor = Double.parseDouble(input.nextLine().replace(",", "."));
+            if (valor < 0) {
+                System.out.println("Programa encerrado!\nSoma final: " + soma);
+                continue;
+            }
+            soma += valor;
+            System.out.println("Soma dos números digitados: " + soma);
+            System.out.println("===============================");
+        } while (valor >= 0);
+
+        input.close();
+    }
+}
