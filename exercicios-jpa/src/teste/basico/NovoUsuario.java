4587c5428275874def769f3e88d0e544c24d15ab
Julio de Brito <juliobrito.tec@gmail.com>
2024-04-11 13:52:49 -0300

    feat: add Usuario.java e NovoUsuario.java em jpa

---
+++ exercicios-jpa/src/teste/basico/NovoUsuario.java
@@ -0,0 +1,28 @@
+package teste.basico;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.basico.Usuario;
+
+public class NovoUsuario {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.
+				createEntityManagerFactory("exercicios-jpa");
+		EntityManager em = emf.createEntityManager();
+
+		Usuario novoUsuario = new Usuario("Coder", "teste@lanche.com.br");
+
+		em.getTransaction().begin();
+		em.persist(novoUsuario);
+		em.getTransaction().commit();
+
+		System.out.printf("O id gerado foi: %d\n", novoUsuario.getId());
+
+		em.close();
+		emf.close();
+	}
+}
